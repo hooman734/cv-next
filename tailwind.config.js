446653d6ed5a8a61f47aa09e5f9cd55a936a2ff3
hooman734 <hesamyan@outlook.com>
2020-12-11 10:20:24 +0400

    correct tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -439,6 +439,7 @@ module.exports = {
       none: 'none',
       disc: 'disc',
       decimal: 'decimal',
+      square: 'square'
     },
     margin: (theme, { negative }) => ({
       auto: 'auto',
